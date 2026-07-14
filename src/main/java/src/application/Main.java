1367e39a475eeb63a92968db30d0eb33e1665b55
luizfernando <lf.nandooliveeira@gmail.com>
2026-07-13 22:57:43 -0300

    testing findByDepartment

--- src/main/java/src/application/Main.java
+++ src/main/java/src/application/Main.java
@@ -14,10 +14,16 @@ public class Main {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
 
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        for (Seller obj : sellerDao.findByDepartment(
+                department)) {
+            System.out.println(obj);
+        }
 
     }
 }
