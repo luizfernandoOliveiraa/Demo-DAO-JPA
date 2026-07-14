fa2d8365183f40858022c50a96ec5994b3b09f06
luizfernando <lf.nandooliveeira@gmail.com>
2026-07-13 22:10:09 -0300

    seller entity class

--- src/main/java/src/application/Main.java
+++ src/main/java/src/application/Main.java
@@ -1,13 +1,19 @@
 package src.application;
 
 import src.model.entities.Department;
+import src.model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
 
     static void main() {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
 
+        Seller seller = new Seller(2000.0, new Date(), obj, "bob@gmail.com", 1, "Bob");
+
+        System.out.println(obj);
+        System.out.println(seller);
     }
 }
