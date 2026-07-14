3bd8717c6d41265acd52385ae957525147d500ec
luizfernando <lf.nandooliveeira@gmail.com>
2026-07-13 22:03:23 -0300

    Department entity class

---
+++ src/main/java/src/application/Main.java
@@ -0,0 +1,13 @@
+package src.application;
+
+import src.model.entities.Department;
+
+public class Main {
+
+    static void main() {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
+    }
+}
