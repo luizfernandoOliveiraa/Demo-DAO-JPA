487fc98bdea18e3e81620931fd8771044a9f17ea
luizfernando <lf.nandooliveeira@gmail.com>
2026-07-13 22:13:16 -0300

    Department Dao and Seller Dao

---
+++ src/main/java/src/model/dao/DepartmentDao.java
@@ -0,0 +1,18 @@
+package src.model.dao;
+
+import src.model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+
+    void update(Department ob);
+
+    void deleteById(Integer id);
+
+    Department findById(Integer id);
+
+    List<Department> findAll();
+}
